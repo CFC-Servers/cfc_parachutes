ab1b452393686354f3be8c172b89128b7a14d09e
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-08-29 20:23:52 -0600

    Fix non-replicated convars

--- lua/cfc_parachutes/shared/sh_parachute_convars.lua
+++ lua/cfc_parachutes/shared/sh_parachute_convars.lua
@@ -43,6 +43,13 @@ CFC_Parachute.DesignMaterialProxyInfo = { -- Proxy info, indexed by material nam
 }
 
 
+CreateConVar( "cfc_parachute_fall_speed", 200, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Target fall speed while in a parachute.", 0, 50000 )
+CreateConVar( "cfc_parachute_fall_lerp", 2, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "How quickly a parachute will reach its target fall speed. Higher values are faster.", 0, 100 )
+CreateConVar( "cfc_parachute_horizontal_speed", 80, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "How quickly you move in a parachute.", 0, 50000 )
+CreateConVar( "cfc_parachute_horizontal_speed_limit", 700, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Max horizontal speed of a parachute.", 0, 50000 )
+CreateConVar( "cfc_parachute_sprint_boost", 1.25, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "How much of a horizontal boost you get in a parachute while sprinting.", 1, 10 )
+CreateConVar( "cfc_parachute_handling", 4, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Improves parachute handling by making it easier to brake or chagne directions. 1 gives no handling boost, 0-1 reduces handling.", 0, 10 )
+
 CreateConVar( "cfc_parachute_space_equip_sv", 1, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Press spacebar while falling to quickly equip a parachute. Defines the default value for players.", 0, 1 )
 CreateConVar( "cfc_parachute_space_equip_double_sv", 0, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Double tap spacebar to equip parachutes, instead of a single press. Defines the default value for players.", 0, 1 )
 
